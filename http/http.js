d6c9cec1ab80b5f62adcc5ca5c26e0ee96c3e8a1
pluderes <kingofpoppro@gmail.com>
2021-09-24 21:10:35 +0700

    master

--- http/http.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("hello baby");
-});
-
-server.listen(5000);
-console.log("nodeJS web sever....");
